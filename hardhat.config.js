1f29a262d6c6fb453cff539fca0d792d8df14799
cielo2021 <hermevillaparedes@gmail.com>
2022-12-23 15:15:18 -0500

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,59 @@
+require("@nomicfoundation/hardhat-toolbox")
+require("@nomicfoundation/hardhat-chai-matchers")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+require("dotenv").config()
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "0x"
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x"
+
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "0x"
+
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "0x"
+
+module.exports = {
+    // solidity: "0.8.17",
+    solidity: {
+        compilers: [{ version: "0.8.17" }, { version: "0.6.0" }],
+    },
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+    },
+    goerli: {
+        url: GOERLI_RPC_URL,
+        accounts: [PRIVATE_KEY],
+        chainId: 5,
+        blockConfirmations: 6,
+        player: {
+            default: 1,
+        },
+    },
+    gasReporter: {
+        enabled: false,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        // coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+            1: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+}
